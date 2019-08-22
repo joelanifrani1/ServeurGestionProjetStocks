7a3f19c9eed5668418959d302e070d84f61fe185
joelanifrani1 <joelanifrani@gmail.com>
2019-08-22 17:30:56 +0000

    first commit

---
+++ src/main/java/com/example/joel/ServeurGestionProjetStocksApplication.java
@@ -0,0 +1,13 @@
+package com.example.joel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurGestionProjetStocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurGestionProjetStocksApplication.class, args);
+	}
+
+}
